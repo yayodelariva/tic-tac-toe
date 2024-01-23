c6b42cf003a5afc810e8d0d564137df574decad6
Yayodelariva <eduardodlrp@outlook.com>
2024-01-23 13:56:46 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id=square-${index}">${square}</div>`
+        })
+        document.querySelector("#gameboard").innerhtml = boardHTML;
+    }
+    
+
+    return {
+        render
+    }
+
+
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+     const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+     }
+     return {
+        start
+     }
+
+    
+
+})();
+
+const startButton = document.querySelector('#start-button')
+startButton.addEventListener("click", ()=> {
+    Game.start();
+})
